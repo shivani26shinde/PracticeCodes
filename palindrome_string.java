d663f864088e9334b8c686b7bf08a9bc6430875a
shivani26shinde <shindeshivani26@gmail.com>
2017-02-14 23:03:21 -0600

    String is a palindrome or not

---
+++ palindrome_string.java
@@ -0,0 +1,33 @@
+# Palindrome String
+
+public class Solution {
+
+	public int isPalindrome(String a) {
+	
+		# replacing the string to lowercase
+	    String s = a.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();;
+	    int n = s.length();    
+	    int start =0;
+	    int end = n - 1;
+		
+		# run the loop till it reaches half the length of the string
+	    while(start < end)
+	    {	
+			# two characters don't match, its not a palindrome string
+	        if(s.charAt(start) != s.charAt(end))
+	        {
+	            return 0;
+	        }
+			
+			# increment start pointer and decrement end pointer
+	        else
+	        {
+	            start++;
+	            end--;
+	        }
+	    }
+		
+		# string is a palindrome
+	    return 1;
+	}
+}
