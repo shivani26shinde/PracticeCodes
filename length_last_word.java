0c7f47c940a1388d7630894a38e45700b7c26203
shivani26shinde <shindeshivani26@gmail.com>
2017-02-17 15:35:32 -0600

    Code to calculate length of last word

---
+++ length_last_word.java
@@ -0,0 +1,29 @@
+# Length of the last word
+
+public class Solution {
+	public int lengthOfLastWord(final String a) {
+	    int len = a.length();	    
+	    int count = 0;
+	    int max = 0;
+	
+	# Empty string
+	    if(len == 0){
+	        return 0;
+	    }
+	# Iterate over the string
+	    for(int i=0;i<len;i++){
+	        while(i<len && (a.charAt(i) != ' ')){
+	            max++;
+	            i++;
+	        }
+		# Update length of every word
+	        if( max != 0){
+	            count  = max;
+	        }
+		# Only spaces encountered
+	        max = 0;
+	    }
+	# Return length of last word
+	    return count;
+}
+}
