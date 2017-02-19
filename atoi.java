ef61c21d93b4fd4211d2f43ba0d5d8589458206c
shivani26shinde <shindeshivani26@gmail.com>
2017-02-18 21:14:22 -0600

    Update atoi.java

--- atoi.java
+++ atoi.java
@@ -3,7 +3,7 @@
 public class Solution {
 	public int atoi(final String a) {
 	
-	# empty string
+	// empty string
 	    if(a.length() == 0){
 	        return 0;
 	    }
@@ -12,29 +12,29 @@ public class Solution {
 	    double res = 0;
 	    Character c = '0';
 		
-	# Avoid whitespaces
+	// Avoid whitespaces
 	    while(i < a.length() && a.charAt(i) == ' '){
 	        i++;
 	    }
 		
-	# Check (-)ve or (+)ve sign
+	// Check (-)ve or (+)ve sign
 	    if(i < a.length() && a.charAt(i) == '-' || i < a.length() && a.charAt(i) == '+'){
 	        c = a.charAt(i);
 	        i++;
 	    }
 		
-	# Numeric digits
+	// Numeric digits
 	    while (i < a.length() && Character.isDigit(a.charAt(i))) {
     		res = res * 10 + Character.getNumericValue(a.charAt(i));
     		i++;
     	}
 		
-	# Add sign
+	// Add sign
     	if(c.equals('-')){
     	   res = -res; 
     	}
     
-	# Check for condition
+	// Check for condition
 	    if(res != 0){
 	        if (res > Integer.MAX_VALUE){
 	            return Integer.MAX_VALUE;
