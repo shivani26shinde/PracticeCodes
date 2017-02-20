364c256f54d6e6e221540685abcb0d69e840bfaa
shivani26shinde <shindeshivani26@gmail.com>
2017-02-20 01:43:50 -0600

    Update length_last_word.java

--- length_last_word.java
+++ length_last_word.java
@@ -6,24 +6,24 @@ public class Solution {
 	    int count = 0;
 	    int max = 0;
 	
-	# Empty string
+	//Empty string
 	    if(len == 0){
 	        return 0;
 	    }
-	# Iterate over the string
+	//Iterate over the string
 	    for(int i=0;i<len;i++){
 	        while(i<len && (a.charAt(i) != ' ')){
 	            max++;
 	            i++;
 	        }
-		# Update length of every word
+		//Update length of every word
 	        if( max != 0){
 	            count  = max;
 	        }
-		# Only spaces encountered
+		//Only spaces encountered
 	        max = 0;
 	    }
-	# Return length of last word
+	//Return length of last word
 	    return count;
 }
 }
