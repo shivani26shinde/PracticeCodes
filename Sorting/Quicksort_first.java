a08e1d6f0a008a6361fca325e32f15c57c8f3d5b
shivani26shinde <shindeshivani26@gmail.com>
2017-02-23 12:25:40 -0600

    Two codes for quicksort added

---
+++ Sorting/Quicksort_first.java
@@ -0,0 +1,43 @@
+# Quicksort (First element is always the pivot)
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n  = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++){
+			arr[i] = sc.nextInt();
+		}
+		int first = 0;
+		int last = n-1;
+		Quicksort(arr, first, last);
+		for(int i=0;i<n;i++){
+			System.out.print(arr[i]+ " ");
+		}
+	}
+	public static void Quicksort(int arr[], int first, int last){
+		if(first < last){
+			int pivot = Divide(arr, first, last);
+			Quicksort(arr, first, pivot-1);
+			Quicksort(arr, pivot+1, last);
+		}
+	}
+	public static int Divide(int arr[], int first, int last){
+		int pivot = arr[first];
+		int temp = last+1;
+		for(int i=last;i>first;i--){
+			if(pivot <= arr[i]){
+				temp--;
+				int val = arr[temp];
+				arr[temp] = arr[i];
+				arr[i] = val;
+			}
+		}
+		int val = arr[temp-1];
+		arr[temp-1] = arr[first];
+		arr[first] = val;
+		return temp-1;
+	}
+}
