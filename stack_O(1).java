e04eb889871c23f8805669f7660b4f2dc3e8b269
shivani26shinde <shindeshivani26@gmail.com>
2017-03-04 03:39:40 -0600

    Stack implementation code added

---
+++ stack_O(1).java
@@ -0,0 +1,60 @@
+# Implement stack in O(1)
+
+class Solution {
+    Stack<Integer> st;
+    int min;
+    Solution(){
+        st = new Stack<Integer>();
+    }
+	
+	// push element in the stack
+    public void push(int x){
+       if(st.isEmpty()){
+           min = x;
+           st.push(x);
+       }
+       else if(x < min){
+           st.push(2*x - min);
+           min = x;
+       }
+       else{
+           st.push(x);
+       }
+    }
+	
+	// pop element from the stack
+    public void pop(){
+        if(st.isEmpty()){
+            return;
+        }
+        int temp = st.pop();
+        if(temp < min){
+            min = 2*min - temp;
+        }
+        return;
+    }
+	
+	// return top element from the stack
+    public int top(){
+       if(st.isEmpty()){
+            return -1;
+        } 
+        int temp = st.peek();
+        if(temp < min){
+            return min;
+        }
+        else{
+            return temp;
+        }
+    }
+	
+	// return minimum element from the stack
+    public int getMin(){
+        if(st.isEmpty()){
+            return -1;
+        }
+        else{
+            return min;
+        }
+    }
+}
