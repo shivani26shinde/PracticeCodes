677e55b48329c856ba3fcf63343b2e1e3ba35790
shivani26shinde <shindeshivani26@gmail.com>
2017-03-04 04:37:58 -0600

    Intersection of two sorted arrays code added

---
+++ intersection_arrays_duplicate.java
@@ -0,0 +1,35 @@
+# Intersection of sorted arrays (Considering duplicate elements)
+
+public class Solution {
+	// DO NOT MODIFY THE LISTS
+	public ArrayList<Integer> intersect(final List<Integer> a, final List<Integer> b) {
+
+	    ArrayList<Integer> list = new  ArrayList<Integer>();
+	    int i = 0;
+	    int j = 0;
+	    
+		// Keep iterating till the end of the lists
+	    while(i <= a.size()-1 && j <= b.size()-1){
+		
+			// list 1 has smaller element so, increment its pointer
+            if(a.get(i) < b.get(j)){
+                i++;
+            }
+			
+			// list 2 has smaller element so, increment its pointer
+            else if(a.get(i) > b.get(j)){
+                j++;
+            }
+			
+			// elements match, add to the list
+            else{
+                list.add(b.get(j));
+                j++;
+                i++;
+            }
+	    }
+		
+		// return list
+	    return list;
+	}
+}
