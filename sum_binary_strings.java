48c4176d2b8e60585dbd76902e87cdeda536df12
shivani26shinde <shindeshivani26@gmail.com>
2017-03-06 02:25:16 -0600

    Update sum_binary_strings.java

--- sum_binary_strings.java
+++ sum_binary_strings.java
@@ -4,19 +4,19 @@ public class Solution {
 	public String addBinary(String a, String b) {
 		StringBuilder sb = new StringBuilder();
 	  
-		# Empty strings
+	// Empty strings
 	    if(a.length() == 0 || b.length() == 0){
 	        return null;
 	    }
 		
-		# Add 0's to make the two strings of equal length
+	// Add 0's to make the two strings of equal length
 	    if(a.length() > b.length()){
 	    	while(a.length() - b.length() != 0){
 	    		b = '0' + b;
 	    	}
 	    }
 		
-		# Add 0's to make the two strings of equal length
+	// Add 0's to make the two strings of equal length
 	    else if(b.length() > a.length()){
 	 		while(b.length() - a.length() != 0){
 	 			a = '0' + a;
@@ -26,38 +26,38 @@ public class Solution {
 	    int two = b.length()-1;
 	    Character temp = '0';
 		
-		# Iterate till the start of the strings
+	// Iterate till the start of the strings
 	    while(one>=0 || two>=0){
 			
-			# Function call, perform addition on carry and string 1 value
+		// Function call, perform addition on carry and string 1 value
 	    	int ans = Value(temp, a.charAt(one));
 			
-			# Returned value either 0 or 1
+		// Returned value either 0 or 1
 	    	if(ans == 0 || ans == 1){
 	    		Character t = Character.forDigit(ans%10,2);
 				
-				# Function call, perform addition on previous ans and string 2 value
+			// Function call, perform addition on previous ans and string 2 value
 	    		int fans = Value(t, b.charAt(two));
 				
-				# Returned value either 0 or 1 (second function call)
+			// Returned value either 0 or 1 (second function call)
 	    		if(fans == 0 || fans == 1){
 	    			sb.append(fans);
 	    			temp = '0';
 	    		}
 				
-				#Returned value 10 (second function call)
+			// Returned value 10 (second function call)
 	    		else{
 	    			sb.append(fans%10);
 	    			temp = '1';
 	    		}
 	    	}
 			
-			# Returned value 10
+		// Returned value 10
 	    	else{
 	    		temp = '1';
 	    		Character t = Character.forDigit(ans%10,2);
 				
-				# Function call, perform addition on previous ans and string 2 value
+			// Function call, perform addition on previous ans and string 2 value
 	    		int fans = Value(t, b.charAt(two));
 	    		sb.append(fans%10);
 	    	}
@@ -65,15 +65,15 @@ public class Solution {
 	    	two--;
 	    }
 		
-		# Last carry on
+	// Last carry on
 	    if(temp != '0'){
 	    	sb.append(temp);
 	    }
 		
-		# Reverse string and return
+	// Reverse string and return
 	    return sb.reverse().toString();
 	}
-	# Perform the sum operation
+	// Perform the sum operation
 	public static int Value(Character a, Character b){
 	    if(a == '0' && b == '0'){
 	        return 0;
