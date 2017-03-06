43581afdd422acaee748deef41bd852cb3f1089c
shivani26shinde <shindeshivani26@gmail.com>
2017-03-05 23:59:26 -0600

    Update trailing_zeros.java

--- trailing_zeros.java
+++ trailing_zeros.java
@@ -4,7 +4,7 @@ public class Solution {
   
 	public int trailingZeroes(int a) {
 	    int count = 0;
-		# Will keep updating the count till its divisible by 5
+	// Will keep updating the count till its divisible by 5
 	     while(a != 0){
 	         count = count + a/5;
 	         a = a/5;
