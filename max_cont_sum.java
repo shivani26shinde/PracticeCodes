f966fb40b76db961d417793101c89c2a7e2f0edb
shivani26shinde <shindeshivani26@gmail.com>
2017-03-05 23:02:49 -0600

    Update max_cont_sum.java

--- max_cont_sum.java
+++ max_cont_sum.java
@@ -4,18 +4,18 @@ public class Solution {
 	// DO NOT MODFIY THE LIST. 
     public int maxSubArray(final List<Integer> A) {
 	    
-		# set sum to default min value and last to 0
+	// Set sum to default min value and last to 0
 	    int sum = Integer.MIN_VALUE;
 	    int last = 0;
 		
-	    # selecting numbers from array
+	  // Selecting numbers from array
 	    for (int num : A) {
 	        
 	        last = last + num;
 			
 	        sum = Math.max(sum, last);
 			
-			# updating last if less than 0
+	// Updating last if less than 0
             if (last < 0)
 	            last = 0;
 	    }
