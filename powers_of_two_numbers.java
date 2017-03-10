4a390f6e586d0c99baf0453be0aaa502e75a1291
shivani26shinde <shindeshivani26@gmail.com>
2017-03-09 22:33:22 -0600

    Update powers_of_two_numbers.java

--- powers_of_two_numbers.java
+++ powers_of_two_numbers.java
@@ -3,11 +3,11 @@
 
 public class Solution {
     public boolean isPower(int a) {
-		# if the given number is one return true 
+	// if the given number is one return true 
         if(a==1){
             return true;
         }
-		# Only run the loop till the value is less than squareroot of the given number i.e. the given number can be at max a square of some number
+	// Only run the loop till the value is less than squareroot of the given number i.e. the given number can be at max a square of some number
         for (int i=2; i<=Math.sqrt(a); i++)
         {
             int j = 2;
