da94701120cac5b3e56b219e99b67fbbf1a8fd1f
shivani26shinde <shindeshivani26@gmail.com>
2017-03-09 22:36:42 -0600

    Update rotated_search_array.java

--- rotated_search_array.java
+++ rotated_search_array.java
@@ -4,12 +4,12 @@ public class Solution {
 	// DO NOT MODIFY THE LIST
 	public int search(final List<Integer> a, int b) {
 		
-		# empty list
+	// empty list
 	    if(a.size() == 0){
 	        return -1;
 	    }
 		
-		# size of the list is 1
+	// size of the list is 1
 	    if(a.size() == 1){
 	        if(a.get(0) == b){
 	            return 0;
@@ -23,16 +23,16 @@ public class Solution {
 	    int end = a.size() - 1;
 	    int ans = -1;
 		
-		# binary search of the element
+	// binary search of the element
 	    while(start <= end){
 	        int mid = (end+start)/2;
 			
-			# element found
+	// element found
             if(a.get(mid) == b){
                  return mid;
             }
 			
-			# check if the element in the 1st half
+	// check if the element in the 1st half
             else if(a.get(start) <= a.get(mid)){
             	if(a.get(start) <= b && b < a.get(mid)){
             		end = mid - 1;
@@ -42,7 +42,7 @@ public class Solution {
             	}
             }
 			
-			# check if the element in the second half
+	// check if the element in the second half
             else{
             	if(a.get(mid) < b && b <= a.get(end)){
             		start = mid + 1;
