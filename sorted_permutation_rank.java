e88551c8944976e0073e030453f9dd20257454c3
shivani26shinde <shindeshivani26@gmail.com>
2017-03-11 23:32:10 -0600

    Update sorted_permutation_rank.java

--- sorted_permutation_rank.java
+++ sorted_permutation_rank.java
@@ -2,56 +2,56 @@
 
 public class Solution {
 	public int findRank(String a) {
-	    int len = a.length();
+		int len = a.length();
 		
-		# length of the string is 1
-	    if(len==1){
-	        return 1%1000003;
-	    }
+	// length of the string is 1
+		if(len==1){
+			return 1%1000003;
+		}
 		
-		# Store the string in chaacter array
-	    ArrayList<String> list = new ArrayList<String>();
-	    Character c[] = new Character[len];
-	    for(int i=0; i<len; i++){
-	        c[i] = a.charAt(i);
-	    }
+	// Store the string in chaacter array
+		ArrayList<String> list = new ArrayList<String>();
+		Character c[] = new Character[len];
+		for(int i=0; i<len; i++){
+			c[i] = a.charAt(i);
+		}
 		
-		# Sort the string in lexicographic order
-	    Arrays.sort(c);
-        StringBuilder sb = new StringBuilder(c.length);
-        for (char i : c) sb.append(i);
-        String fc = sb.toString();
+	// Sort the string in lexicographic order
+		Arrays.sort(c);
+		StringBuilder sb = new StringBuilder(c.length);
+		for (char i : c) sb.append(i);
+		String fc = sb.toString();
 		
-		# Calling the permute function
-        permute(c, 0, list);
-        int ans = list.indexOf(a);
-        return ((ans+1)%1000003);
+	// Calling the permute function
+		permute(c, 0, list);
+		int ans = list.indexOf(a);
+		return ((ans+1)%1000003);
 	}
 	public static void permute(Character c[], int val, ArrayList<String> list){
 	
-		# End of the array
+		// End of the array
 		if(val >= c.length - 1){
 		
-			# Appending the string to the list
+			// Appending the string to the list
 			StringBuilder sb = new StringBuilder(c.length);
 			for (char i : c) sb.append(i);
-        	String fc = sb.toString();
-        	list.add(fc);
-        	return;
+			String fc = sb.toString();
+			list.add(fc);
+			return;
 		}
 		for(int i=val; i<c.length; i++){
-			# Swapping the two elements
-      		Character j = c[val];
-      		c[val] = c[i];
-      		c[i] = j;
+		// Swapping the two elements
+			Character j = c[val];
+			c[val] = c[i];
+			c[i] = j;
 			
-			# Recursively calling the permute function
-      		permute(c, val+1, list);
+		// Recursively calling the permute function
+      			permute(c, val+1, list);
 			
-			# Reswappinmg the two elements 
-      		j = c[val];
-      		c[val] = c[i];
-      		c[i] = j;
+		// Reswappinmg the two elements 
+			j = c[val];
+			c[val] = c[i];
+			c[i] = j;
 		}
 	}
 }
