8dcbd7a50cab7d966f53ab6e82e5b50b464bc555
shivani26shinde <shindeshivani26@gmail.com>
2017-03-26 20:32:32 -0500

    Small update

--- StringPermutation.java
+++ StringPermutation.java
@@ -1,12 +1,9 @@
 # All the permutations of a string in another string
 
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class StringPermutation
 {
 	public static void main (String[] args) throws java.lang.Exception
