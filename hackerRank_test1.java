886b8431760fd83c8163e490ae3ec71c18b3df7e
shivani26shinde <shindeshivani26@gmail.com>
2017-09-16 13:07:50 -0500

    Update

---
+++ hackerRank_test1.java
@@ -0,0 +1,37 @@
+// Find the maximum degree element with minimum sub-array length
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int[] array = {1,1,1,1,2,2,1,2,2,2};
+		HashMap<Integer, Integer> map = new HashMap<>();
+		HashMap<Integer, Integer> start = new HashMap<>();
+		HashMap<Integer, Integer> end = new HashMap<>();
+		for(int i=0;i<array.length;i++){
+			if(map.containsKey(array[i])){
+				int val = map.get(array[i]);
+				map.put(array[i], val+1);
+				end.put(array[i], i);
+			}
+			else{
+				map.put(array[i], 1);
+				start.put(array[i], i);
+			}
+		}
+		int maxVal = Collections.max(map.values());
+		ArrayList<Integer> list = new ArrayList<>();
+		for(Integer i: map.keySet()){
+			if(map.get(i) == maxVal){
+				list.add(i);
+			}
+		}
+		ArrayList<Integer> minVal = new ArrayList<>();
+		for(int i=0;i<list.size();i++){
+			int s = start.get(list.get(i));
+			int e = end.get(list.get(i));
+			minVal.add(e-s);
+		}
+		System.out.println(Collections.min(minVal));
+	}
+}
